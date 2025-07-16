9ae0acc77e66483b68eebc3670fe5a9b93c1dd3d
Jason <jason.vbnguyen@gmail.com>
2025-07-15 19:03:03 -0500

    renamed sql file

--- tables.sql
+++ tables.sql
@@ -9,6 +9,18 @@ CREATE TABLE `admins` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- HoursTracker.events definition
+
+CREATE TABLE `events` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `date` date DEFAULT NULL,
+  `hours` int NOT NULL DEFAULT '0',
+  `desc` text,
+  `needproof` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE entries(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	event_id INTEGER,
